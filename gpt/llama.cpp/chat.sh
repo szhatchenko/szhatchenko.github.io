1b6eca175858c3757ae2f5d41f4459d5f35d1fa0
Sergey Zhatchenko <zha@DevelopmentOnTheEdge.com>
2023-04-09 18:21:30 +0700

    update

--- gpt/llama.cpp/chat.sh
+++ gpt/llama.cpp/chat.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+echo Model: $1
+
 while read MSG; do 
-  RESP=$(./main-avx-avx2-fma-f16c-sse3 -m ggml-model-7b-q4_0.bin -p "${MSG}" -n 512 2>&1 | grep -o -P '(?<=Выход:).*(?=\[end of text\])')
+  RESP=$(./main-avx-avx2-fma-f16c-sse3 -m models/$1 -p "${MSG}" -n 512 2>&1 | grep -o -P '(?<=Выход:).*(?=\[end of text\])')
   echo '> [1m[32m[0m' $RESP '[0m'
 done
